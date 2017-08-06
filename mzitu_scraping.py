237112ed1a84c88dee379438c0241230b9339e82
firejq <firejq@outlook.com>
2017-08-07 00:45:36 +0800

    Some changes

--- mzitu_scraping.py
+++ mzitu_scraping.py
@@ -1,4 +1,4 @@
-import asyncio
+# 同步IO -- 多线程版本
 import os
 import random
 import time
@@ -16,6 +16,7 @@ import time
 #         # print(i.strip())
 #     return iplist
 from multiprocessing import Queue, Process
+from threading import Thread
 
 import requests
 from bs4 import BeautifulSoup
@@ -111,88 +112,21 @@ def get(url, timeout=None, proxy=None, num_retries=6, extra=None):
                 return get(url, 3)
 
 
-async def img_get(url, timeout=None, proxy=None, num_retries=6, extra=None):
-    '''
-    自定义请求逻辑：
-    先尝试直接连接请求，随机使用User-Agent，
-    若请求失败，重试6次，
-    若重试6次之后仍旧失败，开始使用代理ip，
-    若使用代理ip请求失败，则随机更换代理ip，最高尝试更换6次代理ip，
-    更换6次代理ip后仍旧请求失败，放弃使用代理，延长timeout直接连接
-    :param url:
-    :param timeout:
-    :param proxy:
-    :param num_retries:
-    :return:
-    '''
-    UserAgents = [
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3072.0 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36 OPR/37.0.2178.32',
-        ''
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586',
-        'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 BIDUBrowser/8.3 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.9.2.1000 Chrome/39.0.2146.0 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36 Core/1.47.277.400 QQBrowser/9.4.7658.400',
-        'Mozilla/5.0 (Linux; Android 5.0; SM-N9100 Build/LRX21V) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile Safari/537.36 MicroMessenger/6.0.2.56_r958800.520 NetType/WIFI',
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Mobile/11D257 QQ/5.2.1.302 NetType/WIFI Mem/28']
-    headers = {'User-Agent': random.choice(UserAgents)}
-    if extra is not None:
-        headers[extra[0]] = extra[1]
-    if proxy is None:
-        try:
-            return requests.get(url, headers=headers, timeout=timeout)
-        except BaseException:
-            if num_retries > 0:  # num_retries:限定的重试次数
-                print(u'获取网页出错，10秒后将重新获取，倒数第：',
-                      num_retries, u'次')
-                time.sleep(10)
-                return get(url, timeout, num_retries=(num_retries - 1))
-            else:
-                print(u'重试6次失败，开始使用代理')
-                time.sleep(10)
-                iplist = getips()
-                IP = ''.join(str(random.choice(iplist)).strip())
-                proxy = {'http': IP}
-                return get(url, timeout, proxy)
-    else:
-        try:
-            iplist = getips()
-            IP = ''.join(str(random.choice(iplist)).strip())
-            proxy = {'http': IP}
-            print(u'当前代理是：', proxy)
-            return requests.get(url, headers=headers, proxies=proxy)
-        except BaseException:
-            if num_retries > 0:
-                time.sleep(10)
-                iplist = getips()
-                IP = ''.join(str(random.choice(iplist)).strip())
-                proxy = {'http': IP}
-                print(u'使用代理获取网页失败，正在更换代理，10秒后将重新获取倒数第',
-                      num_retries, u'次')
-                print(u'当前代理是：', proxy)
-                return get(url, timeout, proxy, num_retries - 1)
-            else:
-                print(u'更换6次代理仍旧失败，代理可能已失效，取消代理')
-                return get(url, 3)
+def img_download(img_queue, topic_url):
+    while not img_queue.empty():
+        res_img = img_queue.get()
 
-
-async def img_download(img_url, topic_url):
-    # 为图片起名
-    name = str(img_url).split('/')[-1]
-    # 向图片地址发起请求并获取response
-    extra = ['Referer', topic_url]
-    img = await img_get(url=img_url, extra=extra)
-    # 在本地以追加模式创建二进制文件
-    f = open(name, 'ab')
-    # 将response的二进制内容写入到文件中
-    f.write(img.content)
-    # 关闭文件流对象
-    f.close()
+        # 为图片起名
+        name = str(res_img).split('/')[-1]
+        # 向图片地址发起请求并获取response
+        extra = ['Referer', topic_url]
+        img = get(url=res_img, extra=extra)
+        # 在本地以追加模式创建二进制文件
+        f = open(name, 'ab')
+        # 将response的二进制内容写入到文件中
+        f.write(img.content)
+        # 关闭文件流对象
+        f.close()
 
 
 def topic_download(topic_queue):
@@ -233,7 +167,7 @@ def topic_download(topic_queue):
                     res_soup = BeautifulSoup(html, 'lxml')
                     continue
 
-            img_list = []
+            img_queue = Queue(maxsize=500)
             for page in range(1, int(max_span) + 1):
                 page_url = r['href'] + '/' + str(page)
 
@@ -251,16 +185,22 @@ def topic_download(topic_queue):
                         time.sleep(3)
                         res_soup = BeautifulSoup(get(page_url).text, 'lxml')
                         continue
-                # 将该主题的所有url放到列表中
-                img_list.append(res_img)
+                # 将该主题的所有url放到队列中
+                img_queue.put(res_img)
 
             begin_time = time.time()
-            loop = asyncio.get_event_loop()
-            tasks = [img_download(img_url, r['href']) for img_url in img_list]
-            # print('开始进入事件循环')
-            loop.run_until_complete(asyncio.wait(tasks))
-            # print('事件循环结束')
-            loop.close()
+            # 多线程下载队列中的所有url
+            thread_number = 16
+            for thread_number_i in range(thread_number):
+                thread = Thread(
+                    target=img_download, args=(
+                        img_queue, r['href']))
+                thread.start()
+                thread.join()
+
+            while not img_queue.empty():
+                time.sleep(0.5)
+                continue
             print(r['href'], r['text'], '爬取完毕，耗时【'
                   + str(time.time() - begin_time) + '】秒')
 
@@ -324,3 +264,4 @@ if __name__ == '__main__':
         print('第', index, '页爬取完毕')
     print('全站爬取完毕')
     exit()
+
